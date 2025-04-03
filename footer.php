121f8f31d030555b77c8edf2661b4d28c70d3592
Rikkette <darte.helene@gmail.com>
2025-04-03 17:40:04 +0200

    footer ok

--- footer.php
+++ footer.php
@@ -1,39 +1,50 @@
 <footer>
 
-    <h5 class="titre_mention_legale">
-        Mention Légale
-    </h5>
+    <div class="mentions-container">
+        <p class="mention_legale">Mentions légales</p>
+        <p class="condition_generale_vente">Conditions Générales de Vente</p>
+    </div>
 
-    <h5 class="titre_mention_legale">
-        Condition Générale de Vente
-    </h5>
 
     <h5 class="titre_reseaux">
         Mes Réseaux
     </h5>
 
-    <div class="icone">
-        
-        <!---------- Instagram ------------>
-        <a href="https://www.instagram.com/astral.ium/?hl=fr" target="_blank">
-            <img src="Style/logo/insta_logo.png" alt="Instagram" class="instagram" width="100" height="100">
-        </a>
+    <div class="reseaux-container">
+        <div class="icone">
+            <!---------- Instagram ------------>
+            <a href="https://www.instagram.com/astral.ium/?hl=fr" target="_blank">
+                <img src="Style/logo/insta_logo.png" alt="Instagram" class="instagram" width="100" height="100">
+            </a>
 
-        <!------------FaceBook------------->
-        <a href="https://www.facebook.com/Astralium.creations.illustrations/" target="_blank">
-            <img src="Style/logo/fb_logo.png" alt="Facebook" class="facebook" width="100" height="100">
-        </a>
+            <!------------FaceBook------------->
+            <a href="https://www.facebook.com/Astralium.creations.illustrations/" target="_blank">
+                <img src="Style/logo/fb_logo.png" alt="Facebook" class="facebook" width="100" height="100">
+            </a>
 
-        <!-------------TIKTOK------------->
-        <a href="https://www.tiktok.com/@astralium" target="_blank">
-            <img src="Style/logo/tiktok_logo.png" alt="TikTok" class="tiktok" width="100" height="100">
-        </a>
+            <!-------------TIKTOK------------->
+            <a href="https://www.tiktok.com/@astralium" target="_blank">
+                <img src="Style/logo/tiktok_logo.png" alt="TikTok" class="tiktok" width="100" height="100">
+            </a>
 
-        <!-------------Etsy------------->
-        <a href="https://www.etsy.com/fr/shop/Astralium?ref=shop-header-name&listing_id=1158499326&from_page=listing" target="_blank">
-            <img src="Style/logo/etsy_logo.webp" alt="Etsy" class="etsy" width="100" height="100">
-        </a>
+            <!-------------Etsy------------->
+            <a href="https://www.etsy.com/fr/shop/Astralium?ref=shop-header-name&listing_id=1158499326&from_page=listing" target="_blank">
+                <img src="Style/logo/etsy_logo.webp" alt="Etsy" class="etsy" width="100" height="100">
+            </a>
+        </div>
     </div>
 
+    <div class="barre_vertical01"></div>
+    <div class="barre_vertical02"></div>
+
+    <div><h5 class="newsletter">Découvre ma Newsletter</h5></div>
+
+    <div>
+        <img src="/Style/logo/logo.png" alt="logo Astralium" class="logoAstra" width="100" height="100">
+    </div>
+
+    <button class="button" type="button">Je m'abonne!</button>
+
     <div id="rectangle" class="rectangle"></div>
+    
 </footer>
